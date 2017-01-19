3699bb138250412f90f9abc7667757586c667740
mosharrofrubel <rubel007cse@gmail.com>
2017-01-19 21:06:55 +0600

    Hello World

---
+++ src/MyTime.java
@@ -0,0 +1,15 @@
+/**
+ * Created by mosharrofrubel on 1/19/17.
+ */
+public class MyTime {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("im learnign java");
+
+
+    }
+
+
+}
---
+++ src/Myclass.java
@@ -0,0 +1,14 @@
+/**
+ * Created by mosharrofrubel on 1/19/17.
+ */
+public class Myclass {
+
+
+    public static void main(String[] args){
+
+
+        System.out.println("Hello World");
+
+    }
+
+}
